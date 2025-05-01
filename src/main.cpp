b6ef263e069d0267f92aecbe600dc24d5f16e0d3
Anna Semeraro <317383@studenti.unimore.it>
2025-05-01 19:23:02 +0200

    aggiunta funzione di calibrazione

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,38 @@
 #define PHOTO_SENSOR_PIN 34   // Pin a cui è collegato il fototransistor
 #define RED_LED 14            // Pin del LED rosso
 
-unsigned long ledOnTimestamp = 0; // Variabile per memorizzare il timestamp quando il LED è acceso
-const int PHOTO_SENSOR_THRESHOLD_VALUE = 900;   // Soglia per il fototransistor (valore da calibrare in base al circuito)
+unsigned long ledOnTimestamp = 0;                             // Variabile per memorizzare il timestamp quando il LED è acceso
+unsigned long PHOTO_SENSOR_THRESHOLD_VALUE = calibration();   // Soglia per il fototransistor (valore da calibrare in base al circuito)
+
+const int LEN_VECTOR = 10; // Lunghezza del vettore per la calibrazione
+
+unsigned long calibration(){
+  unsigned long photo_calibration_vec[LEN_VECTOR];
+  unsigned long sum = 0;
+  int valid_samples = 0;
+
+
+  for(int i=0; i<LEN_VECTOR; i++){
+      photo_calibration_vec[i] = analogRead(PHOTO_SENSOR_PIN);
+      delayMicroseconds(90);
+  }
+
+  for(int i=0; i<LEN_VECTOR; i++){
+      // if reading 0 = max light, so shift all data to one
+      photo_calibration_vec[i] += 1;
+      if (sum + photo_calibration_vec[i] < sum) {  // Overflow check
+          Serial.println("Overflow detected!");
+          return 0;  // Handle overflow case
+      }
+      sum += photo_calibration_vec[i];
+  }
+
+  //hope this cast the operation 
+  unsigned long threshold = sum / LEN_VECTOR;
+
+  return threshold;
+  
+}
 
 void setup() {
   Serial.begin(9600);
